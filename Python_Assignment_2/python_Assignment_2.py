b8e29a34ac55e1bc99c00e6cfc51a762d9d1297a
henaxb <42467681+henaxb@users.noreply.github.com>
2020-09-19 15:00:29 +0530

    Add files via upload
    2 Assignment of iNeuron.

---
+++ Python_Assignment_2/python_Assignment_2.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Que 1
+# 1. Create the below pattern using nested for loop in Python.
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# In[31]:
+
+
+for i in range(6):
+    print(i*"*")
+for j in range(6,0,-1):
+    print(j*"*")
+
+
+# # Que 2
+# 2. Write a Python program to reverse a word after accepting the input from the user.
+# #Sample Output
+# Input word: ineuron
+# Output: norueni
+
+# In[47]:
+
+
+a=input("Input word :")
+wlist=list(a)
+wlist.reverse()
+print("output: ","".join(wlist))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
